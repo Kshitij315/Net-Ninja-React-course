9fad93e49e0d48769d9a8875023248a1fdb5f901
Kshitij K <kshitijkk15@gmail.com>
2024-09-02 18:33:34 +0530

    anonymous function

--- src/Home.js
+++ src/Home.js
@@ -2,12 +2,20 @@ const Home = () => {
     
 const handleClick = () => {
     console.log('Button clicked!');
-}    
+}  
+
+const handleClickAgain  = (name) => {
+    console.log("Hello "+ name)
+}
     return ( 
 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
+            {/* anonymous function */}
+            <button onClick={() => {
+                handleClickAgain('John Doe')
+            }}>Click me again</button>
         </div>
      );
 }
