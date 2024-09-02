ed5a739fe1ec1c9d3dcf4f2f424222113a53eb22
Kshitij K <kshitijkk15@gmail.com>
2024-09-02 20:05:58 +0530

    using more hooks

--- src/Home.js
+++ src/Home.js
@@ -3,10 +3,13 @@ import { useState } from "react";
 const Home = () => {
 
     const [name,setName] = useState('dalio') //using hooks to change the state of name
-    
+    const [age,setAge] = useState(25)
+    let [num,setNum]=useState(0)
 const handleClick = () => {
     // console.log('Button clicked!',event);
     setName('john');
+    setAge(30);
+    setNum(++num)
 }  
 
 // const handleClickAgain  = (name,e) => {
@@ -16,7 +19,11 @@ const handleClick = () => {
 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name}</p>
+            <div>
+            <p>{name} is {age} years old</p>
+            <p>{num}</p>
+
+            </div>
             <button onClick={handleClick}>Click me</button>
             {/* anonymous function */}
             {/* <button onClick={(e) =>  handleClickAgain('John Doe',e)}>Click me again</button> */}
