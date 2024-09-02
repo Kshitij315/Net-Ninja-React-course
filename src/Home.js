4a6bfb5e4afda5bd0fbb375e172b2ae7e63ad800
Kshitij K <kshitijkk15@gmail.com>
2024-09-02 18:58:06 +0530

    using hooks

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,25 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    const [name,setName] = useState('dalio') //using hooks to change the state of name
     
 const handleClick = () => {
-    console.log('Button clicked!');
+    // console.log('Button clicked!',event);
+    setName('john');
 }  
 
-const handleClickAgain  = (name) => {
-    console.log("Hello "+ name)
-}
+// const handleClickAgain  = (name,e) => {
+//     console.log("Hello "+ name,e.target)
+// }
     return ( 
 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
             <button onClick={handleClick}>Click me</button>
             {/* anonymous function */}
-            <button onClick={() => {
-                handleClickAgain('John Doe')
-            }}>Click me again</button>
+            {/* <button onClick={(e) =>  handleClickAgain('John Doe',e)}>Click me again</button> */}
         </div>
      );
 }
