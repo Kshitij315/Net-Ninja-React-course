ea33ef506bcf60230725eca1ca4ce24423f02de8
Kshitij K <kshitijkk15@gmail.com>
2024-09-03 18:56:34 +0530

    useffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./Bloglist";
 const Home = () => {
 
@@ -11,16 +11,26 @@ const Home = () => {
         
     ]);
 
+const [name,setName]= useState('mario');
+
 const handleDelete = (id) => {
 const newBlogs= blogs.filter(blog => blog.id !==id)
 setBlogs(newBlogs)
 
 }
+// runs on every rerender
+useEffect(() => {
+    console.log('use effect ran');
+    console.log(name);
+    //name dependency
+},[name]);
     return ( 
 
         <div className="home">
          <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
          {/* <BlogList blogs={blogs.filter((blog) => blog.author=== 'mario')} title={"Mario's blogs"}/> */}
+        <button onClick={() => setName('luigi')}>change name</button>
+        <p> {name}</p>
         </div>
      );
 }
