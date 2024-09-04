605a7299ab92a9f7be080547d7ec1312b8920f64
Kshitij K <kshitijkk15@gmail.com>
2024-09-04 23:30:31 +0530

    getting the response object

--- src/Home.js
+++ src/Home.js
@@ -9,19 +9,28 @@ const [isPending,setIsPending]=useState(true);
 
 // runs on every rerender
 useEffect(() => {
-    //timeout to show loading longer
-   setTimeout(() => {
-    fetch("http://localhost:8000/blogs")
-   .then(res => {
-    return res.json();//returns javascript object
- 
-   })
-   .then(data => {
-   setBlogs(data);
-   setIsPending(false);
-   });
-},1000);
-},[]);
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        console.log(res)
+        // if (!res.ok) { // error coming back from server
+        //   throw Error('could not fetch the data for that resource');
+        // } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setBlogs(data);
+        // setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        // setIsPending(false);
+        // setError(err.message);
+        console.log(err.message);
+      })
+    }, 1000);
+  }, [])
     return ( 
 
         <div className="home">
