0c9eae9f0ea72b49554e30af54c6926b85a0dec9
Kshitij K <kshitijkk15@gmail.com>
2024-09-18 17:43:47 +0530

    404 page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import { BrowserRouter as Router , Route,Routes } from 'react-router-dom';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
   const title ='Welcome to the new blog';
@@ -24,6 +25,7 @@ function App() {
 
   <Route path="/create" element={<Create />}></Route>
   <Route path="/blogs/:id" element={<BlogDetails />}></Route>
+  <Route path="*" element={<NotFound/>}></Route>
 
 
 </Routes>
