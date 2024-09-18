f6dab31d14d628acb281486993fa1576be7a6bf5
Kshitij K <kshitijkk15@gmail.com>
2024-09-18 13:16:28 +0530

    deleting blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,25 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
+import { useNavigate } from "react-router-dom";
+
+
 const BlogDetails = () => {
     const {id} =  useParams();
     const { data: blog, error , isPending}=useFetch("http://localhost:8000/blogs/" + id)
+    const navigate=useNavigate();
+
+    const HandleClick = () => {
+        // Delete the blog
+        fetch("http://localhost:8000/blogs/" + blog.id,{
+            method: 'DELETE',
+           
+        })
+        .then(() => {
+            navigate("/")
+        })
+    }
+
     return (
         <div className="blog-details">
             {/* condintional loading */}
@@ -14,9 +30,10 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Writtten by {blog.author}</p>
                     <div>{blog.body} </div>
+                    <button onClick={HandleClick}>Delete</button>
                 </article>
             )}
-        <h2>Blog Details - {id}</h2>
+        
         </div>
       );
 }
